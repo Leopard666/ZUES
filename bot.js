748a120558ba14219f293e1590615ef419dff600
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-08 23:55:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 const Canvas = require("canvas");
-const jimp = require("jimp");
 const moment = require('moment');
 const yt = require('ytdl-core');
 const prefix = '>'
