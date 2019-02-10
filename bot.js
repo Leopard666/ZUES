0297f80389281b05714807765a607b1737709c06
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-10 13:56:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,6 +59,7 @@ client.channels.get("543463470532788274").setName(`THE G`);
 client.channels.get("543463470532788274").setName(`THE`);
 client.channels.get("543463470532788274").setName(`TH`);
 client.channels.get("543463470532788274").setName(`T`);
+client.channels.get("543463470532788274").setName(`THE GRID™`);
 
   }, 20000);
 });
