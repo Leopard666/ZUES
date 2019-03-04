363a5e3daf76ea08f80bee589f13f77516b75cf2
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-03-04 17:52:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -306,6 +306,8 @@ client.on('message', message => {
 
 // ==================================================================
 
+let config;
+
 if (!process.env.DISCORD_TOKEN) {
   config = require('./settings.json');
 }
@@ -390,95 +392,6 @@ function updatePresence(member) {
     }
   });
 }
-// ==================================================================
-
-let config;
-
-if (!process.env.DISCORD_TOKEN) {
-  config = require('./settings2.json');
-}
-
-let listenBot, listenBotName, messageChannel;
-let timeSinceOffline = undefined;
-
-client.on('ready', () => {
-  listenBot = client.guilds.get(process.env.SERVER_ID || config.server_id).members.get(process.env.BOT_ID || config.listen_bot_id);
-  messageChannel = client.channels.get(process.env.CHANNEL_ID || config.notify_channel_id);
-  updatePresence(listenBot);
-
-  if (!listenBot.user.bot) {
-    throw new Error('Specified user is not a bot. Make sure the "listen_bot_id" under the settings.json file has the correct bot id.');
-  }
-
-  console.log('Service started.');
-  console.log(`Listening to bot: ${listenBotName}`);
-  console.log(`Will send messages to the ${messageChannel.name} channel.`);
-});
-
-client.on('presenceUpdate', (oldMember, newMember) => {
-  if (oldMember.id === (process.env.BOT_ID || config.listen_bot_id)) {
-    let oldStatus = oldMember.presence.status;
-    let newStatus = newMember.presence.status;
-
-    if (oldStatus == 'dnd' && newStatus === 'offline') {
-      timeSinceOffline = Date.now();
-        messageChannel.send({
-	     embed: new Discord.RichEmbed()
-	    .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('dc322f')
-	    .setFooter('● 🔰 [ THE GRID™ - OFFICIAL - 2019© ] 🔰 ●')
-	    .setTimestamp()
-            .setTitle('**● :robot: [QUORRA] IS OFFLINE NOW !** ')
-	    .setDescription(`
-
-● Attention **[ THE GRID™ ]** Bot Users **[ QUORRA ]** Has Gone :
-**[ Offline ]** 
-
-● If The Bot Has Gone Unexpected Offline, It Could Be 1 Of The Following Errors :
-
-● 1. A Bot/Server Crash.
-● 2. Discord has issues (check if other bots are online or offline).
-● 3. Discord deleted the the bot tokens (owner will fix asap).
-
-● I will post a message in this channel once the bot comes back online so keep checking in this channel or check the user list.
-
-● In the meantime I have notified **[ The Rare Ranger ]** that the bot has gone offline.`)    
-					    
-   })
-      } 
-  }   
-});
-
-client.login(process.env.DISCORD_TOKEN || config.discord_token);
-
-function displayTime(millis) {
-  const totalTime = Math.floor(millis / 1000);
-  const days = Math.floor(totalTime / 86400);
-  const hours = Math.floor(totalTime / 3600) % 24;
-  const minutes = Math.floor(totalTime / 60) % 60;
-  const seconds = totalTime % 60;
-
-  let result = [];
-  const timeArr = [days, 'days', hours, 'hours', minutes, 'minutes', seconds, 'seconds'];
-  for (let i = 0; i < timeArr.length; i+=2) {
-    if (timeArr[i] !== 0) {
-      result.push(timeArr[i], timeArr[i + 1]);
-     }
-  }
-  return result.join(' ');
-}
-
-function updatePresence(member) {
-  listenBotName = member.nickname || member.user.username;
-  client.user.setPresence({
-    game: {
-      name: `${listenBotName}`,
-      type: 'LISTENING'
-    }
-  });
-}
-
 // ==================================================================
 
 client.login(process.env.BOT_TOKEN);
