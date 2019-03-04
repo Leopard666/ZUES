1334a5de9a4eba4adfe3c3653ff594022384a514
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-03-04 17:23:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -378,10 +378,10 @@ function displayTime(millis) {
   for (let i = 0; i < timeArr.length; i+=2) {
     if (timeArr[i] !== 0) {
       result.push(timeArr[i], timeArr[i + 1]);
-    }
-  }
   return result.join(' ');
-}
+   }
+  }
+ } 
   });
 // ==================================================================
 
